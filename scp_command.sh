20ca940db3214f3d3ba4088e864573c36f816e7e
Ali Rahmani <ali93rahmani@gmail.com>
2023-07-03 17:44:47 +0330

    Update scp_command.sh

--- scp_command.sh
+++ scp_command.sh
@@ -1,2 +1,4 @@
 cd <target file>
 scp -Cpv <file_name>.sql  <user:like ::: rahmani>@<ip:like ::: 81.91.151.100>/<path: like ::: alirahmani>/<file_name>.sql
+
+scp  root@postgres.stg.pol.03.miare.ir:/var/lib/postgresql/miare.bak.sql.gz ~/miare.bak.sql.gz
